91f87d13da0f2655358d9b50c8001abdecbf3e82
Vladimir Samoylov <cageyv.dev@gmail.com>
2021-11-12 18:13:04 +0700

    localstack-app: fix compose and js

--- localstack-app/index.js
+++ localstack-app/index.js
@@ -8,7 +8,6 @@ AWS.config.getCredentials(function(err) {
 });
 
 if (process.env.ANAZONAWS_SSM_ENDPOINT) { var ssm_options = {endpoint: process.env.ANAZONAWS_SSM_ENDPOINT} }
-delay(3000).then(() => console.log('ran after 1 second1 passed'));
 
 var ssm = new AWS.SSM(ssm_options);
 var params = {
@@ -17,7 +16,13 @@ var params = {
 };
 
 ssm.getParameter(params, function(err,data) {
-    if (err) console.log(err, err.stack);
-    else     console.log(data)
+    if (err) {
+        console.log(err, err.stack)
+        process.exit(1)
+    }
+    else  {
+        console.log(data)
+        process.exit(0)
+    }   
 });
 
